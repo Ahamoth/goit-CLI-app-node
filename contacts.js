077fc393613018781761cef2aeb9eb527f471612
Ahamoth <beeminer.store@gmail.com>
2023-05-23 17:20:49 +0300

    edit

--- contacts.js
+++ contacts.js
@@ -1,8 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
-const { customAlphabet } = require("nanoid");
-const alphabet = "0123456789";
-const nanoid = customAlphabet(alphabet, 3);
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
